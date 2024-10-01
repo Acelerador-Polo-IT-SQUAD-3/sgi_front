57214899d74711739564418b57ef95c1d638773e
Yanina Martin <yaninamicamartin@gmail.com>
2024-10-01 10:58:08 -0300

    acceso al menu

--- front_end/src/vite-env.d.ts
+++ front_end/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly API_URL: string;
+  // define otras variables de entorno aquí
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
