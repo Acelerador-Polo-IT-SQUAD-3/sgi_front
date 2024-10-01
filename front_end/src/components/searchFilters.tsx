af1a93cff9a7791f508d437cd9475cddc2f833a9
matias1364 <matiasleoncontreras@gmail.com>
2024-10-01 19:35:34 -0300

    add clear filters

--- front_end/src/components/searchFilters.tsx
+++ front_end/src/components/searchFilters.tsx
@@ -1,9 +1,9 @@
-import React , { useState }from 'react';
+import React, { useState } from 'react';
 import { IonItem, IonList, IonSelect, IonSelectOption, IonTitle, IonButton } from '@ionic/react';
 import { Button } from '@mui/material';
 
 interface SearchFiltersProps {
-  onSearch: (filters: { role_id?: string; program_id?: string; technology_id?: string }) => void;
+  onSearch: (filters: { role_id?: string; program_id?: string; team_id?: string; technology_id?: string }) => void;
   onAddParticipant: () => void;
 }
 
@@ -11,14 +11,24 @@ const SearchFilters: React.FC<SearchFiltersProps> = ({ onSearch, onAddParticipan
   const [roleId, setRoleId] = useState<string | undefined>(undefined);
   const [programId, setProgramId] = useState<string | undefined>(undefined);
   const [technologyId, setTechnologyId] = useState<string | undefined>(undefined);
+  const [teamId, setTeamId] = useState<string | undefined>(undefined);
 
   const handleSearch = () => {
     const filters = {
       role_id: roleId,
       program_id: programId,
+      team_id: teamId,
       technology_id: technologyId,
     };
     onSearch(filters);
+  };
+
+  const handleClearFilters = () => {
+    setRoleId(undefined);
+    setProgramId(undefined);
+    setTechnologyId(undefined);
+    setTeamId(undefined);
+    onSearch({});
   };
 
   return (
@@ -53,6 +63,19 @@ const SearchFilters: React.FC<SearchFiltersProps> = ({ onSearch, onAddParticipan
         </IonItem>
         <IonItem lines='none' className="rounded-md" color={'dark'}>
           <IonSelect
+            aria-label="Equipo"
+            placeholder="Equipo"
+            value={teamId}
+            onIonChange={(e) => setTeamId(e.detail.value)}
+            className="w-32"
+          >
+            <IonSelectOption value="1">Team 1</IonSelectOption>
+            <IonSelectOption value="2">Team 2</IonSelectOption>
+            <IonSelectOption value="3">Team 3</IonSelectOption>
+          </IonSelect>
+        </IonItem>
+        <IonItem lines='none' className="rounded-md" color={'dark'}>
+          <IonSelect
             aria-label="Tecnología"
             placeholder="Tecnología"
             value={technologyId}
@@ -66,14 +89,16 @@ const SearchFilters: React.FC<SearchFiltersProps> = ({ onSearch, onAddParticipan
         </IonItem>
         <Button variant="contained" onClick={handleSearch}>Buscar</Button>
       </IonList>
-      <div className='mt-4'>
+      <div className='mt-4 flex justify-between'>
         <IonButton routerLink='/profile/new-user' onClick={onAddParticipant}>Nuevo Participante</IonButton>
-      </div>  
-      </section>
+        <IonButton onClick={handleClearFilters}>Limpiar filtros</IonButton>
+      </div>
+    </section>
   );
 };
 
 export default SearchFilters;
+
 
 
 
