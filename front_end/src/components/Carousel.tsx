73284fa7be318df55af83268fa324af40ac79f65
carminepi <carmin.espinoza@epidataconsulting.com>
2024-10-06 16:29:07 -0400

    Prueba deploy

--- front_end/src/components/Carousel.tsx
+++ front_end/src/components/Carousel.tsx
@@ -14,7 +14,7 @@ import "swiper/css/scrollbar";
 
 export const Carousel = () => {
     const slides = [
-        { src: "assets/img/imagen1.png", alt: "Slide 1" },
+        { src: "https://drive.google.com/file/d/1erCS01FIgOmNxSLwO6_HqB7LCVdTBtD_/view?usp=sharing", alt: "Slide 1" },
         { src: "../src/dummy-images/imagen2.jpg", alt: "Slide 2" },
         { src: "../src/dummy-images/imagen3.jpg", alt: "Slide 3" },
         { src: "../src/dummy-images/imagen4.jpg", alt: "Slide 4" },
