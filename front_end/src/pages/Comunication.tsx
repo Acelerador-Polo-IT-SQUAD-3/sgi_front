99a66c7b8e504312da27e901c25e2107d03aa331
Ludmila Muñoz <51221669+LudmiM@users.noreply.github.com>
2024-10-12 21:24:35 -0300

    Feature page conmunication (#27)
    * Add formcomunication
    
    * Add item's in seled and querys
    
    * Design for page comunication
    
    * Remove .env from tracking

---
+++ front_end/src/pages/Comunication.tsx
@@ -0,0 +1,44 @@
+import { IonContent } from "@ionic/react";
+import React, { useEffect, useState } from 'react';
+import FormComunication from "../components/FormComunication";
+
+const Comunication: React.FC = () => {
+    const apiUrl = import.meta.env.VITE_API_URL;
+    const [data, setData] = useState<Array<{
+        user_id: number;
+        user_name: string;
+        user_email: string;
+        team_names: string;
+    }> | null>(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+
+            const user = JSON.parse(sessionStorage.getItem('user') || '{}');
+            const userId = user?.id;
+            try {
+                const response = await fetch(`${apiUrl}/teams/${userId}`);
+
+                if (!response.ok) {
+                    throw new Error('Error fetching data');
+                }
+
+                const result = await response.json();
+                setData(result);
+
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, [apiUrl]);
+
+    return (
+        <IonContent>
+            <FormComunication data={data} />
+        </IonContent>
+    );
+};
+
+export default Comunication;
