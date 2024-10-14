576c10cbdaa52b91ea592c27eb519b08ebcbc8be
Yanina Martin <yaninamicamartin@gmail.com>
2024-10-13 22:02:15 -0300

    navbar

--- front_end/src/components/Header.tsx
+++ front_end/src/components/Header.tsx
@@ -1,14 +1,13 @@
-
 import React from 'react';
 import { IonButton, IonContent, IonHeader, IonTitle } from '@ionic/react';
 import Sidebar from './Sidebar';
 import Logo from "../dummy-images/polo-it.png";
 
-
+// Definir interfaces
 interface HeaderButton {
   label: string;
-  onClick?: () => void; // Hacer que onClick sea opcional
-  routerLink?: string; // Hacer que routerLink sea opcional
+  onClick?: () => void;
+  routerLink?: string;
 }
 
 interface HeaderProps {
@@ -16,88 +15,62 @@ interface HeaderProps {
   activeSidebar: boolean;
 }
 
+// Función para generar estilos de botones
+const buttonClasses = `
+  w-[120px]
+  h-[40px]
+  bg-[#E9EEF4]
+  gap-[10px]
+  opacity-100
+  text-black
+  text-xs
+  font-poppins
+  text-[16px]
+  font-medium
+  leading-[24px]
+  tracking-[-0.1px]
+  text-center
+`;
+
+// Componente principal Header
 const Header: React.FC<HeaderProps> = ({ buttons, activeSidebar }) => {
   return (
     <>
-      {activeSidebar ? (
-        <>
-          <Sidebar />
+      <IonHeader className="flex justify-between items-center h-78 pr-2 bg-[#326789]">
+        {activeSidebar ? (
+          <>
+            <Sidebar />
+          </>
 
-          <div className="flex justify-end items-end h-12 pr-2 bg-[#326789] ">
-            {buttons.map((button, index) => (
-              <IonButton
-                key={index}
-                onClick={button.onClick}
-                routerLink={button.routerLink}
-                fill="clear"
-                className="
-                w-[120px] 
-              h-[40px] 
-                mt-[13px] 
-                bg-[#E9EEF4] 
-                gap-[10px] 
-                rounded-tl-[10px] 
-                rounded-none 
-                opacity-100
-                text-black
-                rounded-none
-                text-xs
-                font-poppins
-                  text-[16px]
-                  font-medium
-                  leading-[24px]
-                  tracking-[-0.1px]
-                  text-center
-              "
-              >
-                {button.label}
-              </IonButton>
-            ))}
-          </div>
-        </>
-      ) : (
+        ) : (
+          <>
 
-        <IonHeader className="flex justify-between items-center h-78 pr-2 bg-[#326789]">
-          <IonTitle className="flex items-center ml-14">
-            <img
-              src={Logo}
-              alt="PoloIT"
-              style={{ height: "40px", width: "auto" }}
-            />
-          </IonTitle>
-          <div className="flex items-center space-x-6">
-            {buttons.map((button, index) => (
-              <IonButton
-                key={index}
-                onClick={button.onClick}
-                routerLink={button.routerLink}
-                className="
-              w-[120px] 
-              h-[40px]
-              bg-[#E9EEF4] 
-              gap-[10px] 
-              rounded-[10px]
-              opacity-100
-              text-black
-              text-xs
-              font-poppins
-                  text-[16px]
-                  font-medium
-                  leading-[24px]
-                  tracking-[-0.1px]
-                  text-center
-              "
-                fill="clear"
-
-              >
-                {button.label}
-              </IonButton>
-            ))}
-          </div>
-        </IonHeader>
-      )}
+          </>
+        )
+        }
+        <IonTitle className="flex items-center ml-14">
+          <img
+            src={Logo}
+            alt="PoloIT"
+            style={{ height: "40px", width: "auto" }}
+          />
+        </IonTitle>
+        <div className="flex items-center space-x-6">
+          {buttons.map((button, index) => (
+            <IonButton
+              key={index}
+              onClick={button.onClick}
+              routerLink={button.routerLink}
+              fill="clear"
+              className={`${buttonClasses} rounded-[10px]`}
+            >
+              {button.label}
+            </IonButton>
+          ))}
+        </div>
+      </IonHeader>
     </>
-  );
+  )
 };
 
 export default Header;
--- front_end/src/components/Sidebar.tsx
+++ front_end/src/components/Sidebar.tsx
@@ -24,7 +24,7 @@ const Sidebar: React.FC = () => {
 
   const getRole = (user: string | null) => {
     if (user) {
-      let u = JSON.parse(user);
+      const u = JSON.parse(user);
       return u.role_id;
     }
     return null;
@@ -34,7 +34,7 @@ const Sidebar: React.FC = () => {
     try {
       const apiUrl = import.meta.env.VITE_API_URL;
       const response = await fetch(`${apiUrl}/menus/${userRole}`, {
-        method: 'GET',
+        method: "GET",
         headers: {
           "Content-Type": "application/json",
         },
@@ -53,7 +53,7 @@ const Sidebar: React.FC = () => {
 
   useEffect(() => {
     setUserRole(getRole(user));
-  }, []);
+  }, [user]);
 
   useEffect(() => {
     if (userRole) {
@@ -63,15 +63,27 @@ const Sidebar: React.FC = () => {
 
   return (
     <>
-
-      <IonMenu contentId="main-content" style={{ height: '100%' }}>
+      {/* Barra superior con botón de menú */}
+      <IonToolbar className="bg-transparente flex justify-between items-center">
+        <IonButton slot="start" className="bg-[#326789] rounded-md p-2 h-10 w-10 flex items-center justify-center">
+          <IonMenuButton className="text-white text-3xl" />
+        </IonButton>
+        <div id="main-content"></div>
+      </IonToolbar>
+      {/* Menú lateral */}
+      <IonMenu contentId="main-content" style={{ height: "100%" }}>
         <IonContent>
           <IonList id="inbox-list" className="h-full">
-            <IonHeader>
+            {/* Encabezado del menú */}
+            <IonHeader className="bg-[#326789]">
               <IonToolbar>
                 <IonTitle className="bg-white text-black">
                   <div className="flex items-center">
-                    <img src="src/dummy-images/desktop-icon.png" alt="icono desktop" className="w-4 h-4 mr-2" />
+                    <img
+                      src="src/dummy-images/desktop-icon.png"
+                      alt="icono desktop"
+                      className="w-4 h-4 mr-2"
+                    />
                     <span className="font-poppins text-sm font-semibold leading-5 text-left">
                       Mis tareas
                     </span>
@@ -79,15 +91,17 @@ const Sidebar: React.FC = () => {
                 </IonTitle>
               </IonToolbar>
             </IonHeader>
+
+            {/* Menú de opciones dinámico */}
             {menus.map((menu, index) => (
               <IonMenuToggle key={index} autoHide={false}>
                 <IonItem
-                  className={location.pathname === menu.path ? "selected" : ""}
+                  className={location.pathname === menu.path ? "selected bg-gray-200" : ""}
                   routerLink={menu.path}
                   routerDirection="forward"
                   lines="none"
                   detail={false}
-                  color={"transparent"}
+                  color="transparent"
                 >
                   <IonLabel>{menu.name}</IonLabel>
                 </IonItem>
@@ -96,26 +110,6 @@ const Sidebar: React.FC = () => {
           </IonList>
         </IonContent>
       </IonMenu>
-
-
-      <IonHeader className="bg-[#326789]">
-        <IonToolbar>
-          <IonButton
-            slot="start"
-            className="bg-[#326789] rounded-md p-2 h-10 w-10 flex items-center justify-center"
-          >
-            <IonMenuButton className="text-white text-3xl" />
-          </IonButton>
-          <IonTitle className="text-white ml-4">
-
-          </IonTitle>
-        </IonToolbar>
-      </IonHeader>
-
-
-      <div id="main-content" >
-
-      </div>
     </>
   );
 };
